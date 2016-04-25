f1672e925ca866f2c3c6560bd8dc2be2881ca3b2
Ivan Jaramillo <converse_ivan@hotmail.com>
2016-04-25 18:24:06 -0500

    Added final funcionality of the app

--- js/alumnos.js
+++ js/alumnos.js
@@ -13,6 +13,14 @@
                 telefono:$scope.nuevoAlumno.telefono,
                 curso:$scope.nuevoAlumno.curso
             });
+            $scope.formVisibility = false;
+            console.log($scope.formVisibility);
+        };
+        
+        $scope.formVisibility = false;
+        $scope.ShowForm = function(){
+            $scope.formVisibility = true;
+            console.log($scope.formVisibility);
         }
     });
 })();
